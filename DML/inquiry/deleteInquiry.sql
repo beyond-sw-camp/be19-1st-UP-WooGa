276eda0a2b0d52c5638d888f1e085e443ae39345
indy032229@gmail.com <indy032229@gmail.com>
2025-07-31 17:09:12 +0900

    feat: #14 - inquiry 테이블의 문의글 삭제 기능 쿼리를 수정함.

--- DML/inquiry/deleteInquiry.sql
+++ DML/inquiry/deleteInquiry.sql
@@ -1,16 +1,18 @@
--- 특정 사용자의 첫번쨰 문의글 삭제
+-- 특정 사용자의 글을 삭제(user01이 본인의 글을 삭제)
 DELETE
   FROM Inquiry
- WHERE inquiry_id = (SELECT
-                            inquiry_id
-                       FROM Inquiry
-                      WHERE user_id = 'user001'
-                   ORDER BY inquiry_id ASC
-                      LIMIT 0,1
-                    );
+ WHERE inquiry_id = 1 and user_id = 'user01';
+
+-- 관리자가 문의글을 삭제
+DELETE
+  FROM Inquiry
+ WHERE inquiry_id = 1;
 
 -- 특정 날짜 이전 문의글 삭제
  DELETE
    FROM Inquiry
   WHERE created_at < '2025-07-22';
+
+select *
+  from inquiry;
 
