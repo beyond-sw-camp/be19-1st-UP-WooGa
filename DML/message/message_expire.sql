d490829b7ebf836ea082084c3f2f108fabb937a7
Kjandgo <rptmffld0204@gmail.com>
2025-07-31 14:41:36 +0900

    fix: #9 만료시 쪽지 삭제
    주석 제거 + 코드 일부 수정

--- DML/message/message_expire.sql
+++ DML/message/message_expire.sql
@@ -1,19 +1,3 @@
--- 자동만료일 설정 : 메세지 수신파트에서 처리 가능해서 여기 작성하던건 주석처리
--- delimiter //
-
--- CREATE OR REPLACE TRIGGER start_expire_date
---     AFTER UPDATE
---     ON message
---     FOR EACH ROW
--- BEGIN
---     UPDATE message
---        SET OLD.message_expiry_date = DATE_ADD(NOW(), INTERVAL 1 MONTH)
---      WHERE OLD.message_id = NEW.message_id;
--- END //
-
--- delimiter ;
-
--- DROP TRIGGER if EXISTS start_expire_date;
 
 -- 만료시 쪽지 삭제
 -- *******************이벤트 스케쥴러 사용할 때 반드시 이벤트 스케쥴러 켜져있는지 확인해야함!!!!!!!!
@@ -27,7 +11,7 @@ STARTS '00:00:01'
 DO
   DELETE 
     FROM message
-   WHERE message_expiry_date = CURRENT_TIMESTAMP //
+   WHERE message_expiry_date <= CURRENT_TIMESTAMP //
 
 delimiter ;
 
