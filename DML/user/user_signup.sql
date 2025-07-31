96458b6a196731882bfc534172bfbfc361b7a207
김민지 <ghi512151@gmail.com>
2025-07-31 14:56:37 +0900

    feat: #13 - 회원가입 기본 기능 구현

---
+++ DML/user/user_signup.sql
@@ -0,0 +1,20 @@
+-- 일반 사용자 회원가입
+-- 사용자는 아이디, 비밀번호, 주소, 전화번호, 이메일 입력
+-- 가입일자, 마지막로그인일자, 마지막비밀번호변경날짜, 사용자 상태, 신고누적횟수는 default 값으로 자동 설정됨
+
+select * from user;
+
+INSERT INTO User (
+                  user_id
+                , pwd
+                , address
+                , phone_number
+                , email
+)
+VALUES (
+    'test_user'
+  , 'test'
+  , '서울시 동작구'
+  , '010-1234-5678'
+  , 'test@test.com'
+       );
