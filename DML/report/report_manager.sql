56bcbbc5b440bff0350d2b4e7251e2263806a2ee
Kjandgo <rptmffld0204@gmail.com>
2025-07-31 15:40:05 +0900

    no message

--- DML/report/report_manager.sql
+++ DML/report/report_manager.sql
@@ -3,3 +3,22 @@ SELECT
        * 
   FROM report
  WHERE reporter_id = 'user03';
+
+-- 신고 게시물 삭제
+DELETE
+  FROM post
+ WHERE post_id = (SELECT reported_post_id
+                    FROM report
+                   WHERE report_id = 18);
+
+
+
+
+
+
+SELECT * FROM report;
+SELECT * FROM post;
+
+
+
+
