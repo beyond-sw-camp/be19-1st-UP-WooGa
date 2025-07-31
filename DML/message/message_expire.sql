b2a0a6722ac9f36587683c0e67160f1b0f76b765
Kjandgo <rptmffld0204@gmail.com>
2025-07-31 12:18:46 +0900

    feat: #9 쪽지 만료기한 관련
    +만료기한 기본값 null로 변경

--- DML/message/message_expire.sql
+++ DML/message/message_expire.sql
@@ -1,4 +1,4 @@
--- -- 자동만료일 설정 : 메세지 수신파트에서 처리 가능해서 여기 작성하던건 주석처리
+-- 자동만료일 설정 : 메세지 수신파트에서 처리 가능해서 여기 작성하던건 주석처리
 -- delimiter //
 
 -- CREATE OR REPLACE TRIGGER start_expire_date
@@ -16,15 +16,20 @@
 -- DROP TRIGGER if EXISTS start_expire_date;
 
 -- 만료시 쪽지 삭제
+-- *******************이벤트 스케쥴러 사용할 때 반드시 이벤트 스케쥴러 켜져있는지 확인해야함!!!!!!!!
+-- 확인 명령어 : SHOW VARIABLES LIKE 'event%';
+-- 이벤트 스케쥴러 작동 명령어 : SET GLOBAL event_scheduler = ON;
 delimiter //
 
 CREATE EVENT del_expired_message
-ON SCHEDULE EVERY 1 DAY
-STARTS '2025-07-30 00:00:00'
+ON SCHEDULE EVERY 10 SECOND
+STARTS '2025-07-31 12:12:00'
 DO
-  DELETE message
+  DELETE 
+    FROM message
    WHERE message_expiry_date = CURRENT_TIMESTAMP //
 
 delimiter ;
 
+-- 이벤트 스케쥴러 삭제
 -- DROP EVENT if EXISTS del_expired_message;
