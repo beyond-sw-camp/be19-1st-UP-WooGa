4d3da55ac6f353496701c5068bb8f94ab0dade72
leejaeguen <worms0421@gmail.com>
2025-08-01 14:03:02 +0900

    feat: #38 누적신고횟수가 존재하는 게시물 조회 및 누적신고횟수가 3회 이상인 게시물 삭제

---
+++ DML/post/reported_post_delete.sql
@@ -0,0 +1,23 @@
+DELIMITER //
+
+CREATE PROCEDURE delete_reported_posts()
+BEGIN
+START TRANSACTION;
+
+-- 게시물 사진 삭제
+DELETE FROM PostPhoto
+WHERE post_id IN (
+    SELECT post_id FROM Post WHERE report_count >= 3
+);
+
+-- 게시물 삭제
+DELETE FROM Post
+WHERE report_count >= 3;
+
+COMMIT;
+END;
+//
+
+DELIMITER ;
+
+CALL delete_reported_posts();
