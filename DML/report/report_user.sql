5930829b7ae3a58cf46700eb4a57530018fd08ad
Kjandgo <rptmffld0204@gmail.com>
2025-08-01 11:40:50 +0900

    no message

--- DML/report/report_user.sql
+++ DML/report/report_user.sql
@@ -2,7 +2,4 @@
 INSERT INTO Report
 (report_content, reporter_id, report_sort, reported_user_id, reported_post_id)
 VALUES
-('신고내용11', 'user02', 'USER', 'user01', NULL),
-('신고내용22', 'user04', 'USER', 'user03', NULL),
-('신고내용33', 'user08', 'USER', 'user10', NULL),
-('신고내용44', 'user09', 'USER', 'user09', NULL);
+('신고내용11', 'user02', 'USER', 'user01', NULL);
