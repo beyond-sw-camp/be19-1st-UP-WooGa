1e90bea8e06d8c39a581a5df0aaabaf918a2d007
Kjandgo <rptmffld0204@gmail.com>
2025-08-01 10:10:24 +0900

    feat: #22 관리자 신고 관리
    ddl문 일부 수정

--- DML/report/report_manager.sql
+++ DML/report/report_manager.sql
@@ -5,16 +5,17 @@ SELECT
  WHERE reporter_id = 'user03';
 
 -- 신고 처리 상태 변경
-UPDATE report
-   SET report_state = 'RESOLVE'   -- REPORTED,PROGRESSING,RESOLVE,REJECT
- WHERE report_id = 20;
+UPDATE report 
+   SET report_state = 'PROGRESSING'   -- REPORTED,PROGRESSING,RESOLVE,REJECT
+     , report_manager = 'admin02'
+ WHERE report_id = 3;
+
+-- 게시물 신고누적횟수 증가
+UPDATE post
+   SET report_count = report_count + 1
+ WHERE post_id = (SELECT report_id FROM report WHERE report_id = 1);
 
 
-
-
-
-SELECT * FROM report;
-SELECT * FROM post;
 
 
 
