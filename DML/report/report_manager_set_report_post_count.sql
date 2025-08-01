9d7f93f811b05df2d96a4e36ff1c3a2b82abf55c
Kjandgo <rptmffld0204@gmail.com>
2025-08-01 10:18:58 +0900

    feat: #22 제목 일부 수정

--- DML/report/report_manager_set_report_post_count.sql
+++ DML/report/report_manager_set_report_post_count.sql
@@ -1,4 +1,4 @@
 -- 게시물 신고누적횟수 증가
 UPDATE post
    SET report_count = report_count + 1
- WHERE post_id = (SELECT report_id FROM report WHERE report_id = 1);
+ WHERE post_id = (SELECT reported_post_id FROM report WHERE report_id = 1);
