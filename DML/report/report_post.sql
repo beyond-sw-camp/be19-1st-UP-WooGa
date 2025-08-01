ff8338adc65dae0af9086fb0d83df0fb84c64022
Kjandgo <rptmffld0204@gmail.com>
2025-08-01 11:36:06 +0900

    feat: #21 사용자 신고 기능
    사진 추가

--- DML/report/report_post.sql
+++ DML/report/report_post.sql
@@ -2,7 +2,11 @@
 INSERT INTO Report
 (report_content, reporter_id, report_sort, reported_user_id, reported_post_id)
 VALUES
-('신고내용1', 'user01', 'POST', NULL, 1),
-('신고내용2', 'user03', 'POST', NULL, 2),
-('신고내용3', 'user04', 'POST', NULL, 3),
-('신고내용4', 'user07', 'POST', NULL, 4);
+('신고내용1', 'user01', 'POST', NULL, 1);
+
+INSERT INTO ReportPhoto
+(image_url,report_id)
+VALUES
+('https://www.notion.so/ohgiraffers/1-_-CodeBuds-239649136c11808299e5f6676d642d0c',1),
+('https://www.notion.so/ohgiraffers/2-_First-Class-239649136c118096b713c0ef7847335b',1),
+('https://www.notion.so/ohgiraffers/3-_UP-Unstoppable-Power-239649136c11804b81b7f58f8373959d',1);
