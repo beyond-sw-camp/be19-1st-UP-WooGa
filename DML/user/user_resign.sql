2606ea0af3f27534c788aca46566e1eeeedde2d3
김민지 <ghi512151@gmail.com>
2025-08-02 19:50:17 +0900

    fix: #55 - 탈퇴 실행 구문 수정

--- DML/user/user_resign.sql
+++ DML/user/user_resign.sql
@@ -1,6 +1,22 @@
+-- 탈퇴 전 회원 아이디
+select
+    user_id as '회원 아이디'
+  , user_state '회원 상태'
+  from user
+ where user_id = 'user09';
+
+-- 탈퇴
 UPDATE User
 SET user_state = 'RESIGN',
     resign_date = CURRENT_TIMESTAMP
-WHERE user_id = 'user01';
+WHERE user_id = 'user09';
 
-select * from user;
+-- 탈퇴 후 회원 상태
+select
+    user_id as '회원 아이디'
+     , user_state '회원 상태'
+from user
+where user_id = 'user09';
+
+-- 로그인 시도 시 실패함
+CALL LoginAttempt('user09', '1234', '192.168.0.1');
