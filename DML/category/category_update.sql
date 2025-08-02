86b71a3f9ea6bcd5342d8889947baafd439f5fa4
김민지 <ghi512151@gmail.com>
2025-08-02 23:02:19 +0900

    fix: #59 - 카테고리 수정 구문 수정

--- DML/category/category_update.sql
+++ DML/category/category_update.sql
@@ -1,7 +1,12 @@
+-- 카테고리명 확인
+select category_name
+from category
+where category_id = 25;
+
 -- 카테고리명 수정
 UPDATE Category
-SET category_name = '카메라'
+SET category_name = '에어컨'
 WHERE category_id = (SELECT category_id
                      FROM   Category
-                     WHERE  category_name ='키보드'
+                     WHERE  category_name ='에어컨(이동식)'
 );
