b52aac08ee4c29efbb4a4983adf922aa4f0c637f
leejaeguen <worms0421@gmail.com>
2025-08-02 18:12:57 +0900

    fix: #53 신고당한 게시물 조회시 신고 누적 횟수에 따른 내림차순 조회 및 PROCEDURE CALL 분리

--- SETUP/PROCEDURE/category_delete_procedure.sql
+++ SETUP/PROCEDURE/category_delete_procedure.sql
@@ -50,13 +50,3 @@ WHERE category_id = target_id;
 END$$
 
 DELIMITER ;
-
--- 함수 호출
-CALL delete_category((SELECT category_id
-                               FROM Category
-                               WHERE category_name = '스타일러')
-);
-
-select * from Category;
-
-select * from Post;
