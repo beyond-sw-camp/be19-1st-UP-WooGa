508a58bb7e5b03089a06dfa96818fec55c6d2a05
김민지 <ghi512151@gmail.com>
2025-08-02 23:21:12 +0900

    feat: #59 - 카테고리 추가 select 구문 및 시연 gif 추가

--- DML/category/category_insert.sql
+++ DML/category/category_insert.sql
@@ -1,7 +1,31 @@
+-- 상위 카테고리 조회
+SELECT
+    category_id,
+    category_name AS '상위 카테고리 이름'
+FROM Category
+WHERE
+    parent_category_id IS NULL
+ORDER BY
+    category_id;
+
 -- 상위 카테고리 추가
 INSERT INTO Category (category_name, parent_category_id)
 VALUES
     ('의류가전', NULL);
+
+-- '의류가전' 카테고리의 하위 카테고리 조회
+SELECT
+    category_id,
+    category_name AS '하위 카테고리 이름'
+FROM
+    Category
+WHERE
+        parent_category_id = (
+        SELECT category_id
+        FROM Category
+        WHERE category_name = '의류가전'
+    )
+ORDER BY category_id;
 
 -- 하위 카테고리 추가
 INSERT INTO Category (category_name, parent_category_id)
