7372e693ae81cc4b2a2f10123f96a7bb09401ebb
김민지 <ghi512151@gmail.com>
2025-08-04 04:03:20 +0900

    feat: #61 - 신고글 담당자 할당 추가, 처리 상태 변경 조건 수정

--- DML/report/report_manager_set_state.sql
+++ DML/report/report_manager_set_state.sql
@@ -1,5 +1,18 @@
+-- 담당자가 없는 신고글에 본인 담당자로 설정
+UPDATE Report
+SET report_manager = 'admin04',
+    report_state = 'PROGRESSING'
+WHERE report_manager IS NULL;
+
 -- 신고 처리 상태 변경
 UPDATE report 
    SET report_state = 'REJECT'   -- REPORTED,PROGRESSING,RESOLVE,REJECT
-     , report_manager = 'admin04'
- WHERE report_id = 5;
+ WHERE report_id = 3
+   AND report_manager = 'admin04';
+
+-- 신고글 확인
+select
+    report_id as '신고글 번호'
+  , report_manager as '관리자'
+  , report_state as '상태'
+    from report;
