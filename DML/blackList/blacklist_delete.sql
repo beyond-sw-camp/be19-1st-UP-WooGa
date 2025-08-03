1f3ca77b4a05d66781476d992d6ef10df7b3b507
김민지 <ghi512151@gmail.com>
2025-08-04 04:20:41 +0900

    fix: #61 - 블랙리스트 조회 구문 수정

--- DML/blackList/blacklist_delete.sql
+++ DML/blackList/blacklist_delete.sql
@@ -2,3 +2,11 @@
 DELETE
   FROM blacklist
  WHERE blacklist_id = 1;
+
+SELECT
+    user_id AS '블랙리스트 사용자',
+        blacklist_reason AS '사유',
+        blacklist_start_at AS '등록 일시',
+        blacklist_period AS '기간(일)',
+        blacklist_end_at AS '블랙리스트 종료일'
+FROM blacklist;
--- DML/blackList/blacklist_list.sql
+++ DML/blackList/blacklist_list.sql
@@ -1,7 +1,12 @@
 -- 블랙리스트 조회
 SELECT
-       user_id
-     , blacklist_reason
-	  , blacklist_start_at
-	  , blacklist_period
-  FROM blacklist;
+    b.user_id AS '블랙리스트 사용자',
+    u.report_count AS '누적 신고 횟수',
+    b.blacklist_reason AS '사유',
+    b.blacklist_period AS '기간(일)',
+    b.blacklist_start_at AS '등록 일시',
+    b.blacklist_end_at AS '블랙리스트 종료일'
+FROM blacklist b
+JOIN user u ON b.user_id = u.user_id;
+
+
