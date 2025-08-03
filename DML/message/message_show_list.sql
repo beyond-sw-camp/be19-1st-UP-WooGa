90ad8fcd240bbb39617d11d445ee5e3ad422269b
김민지 <ghi512151@gmail.com>
2025-08-04 03:10:10 +0900

    fix: #61 - 쪽지함 구문 수정
    - 받은 쪽지함, 안 읽은 쪽지함, 보낸 쪽지함 구분
    - select에 sender)id, sent_at, message_expiry_date, check_time 추가

--- DML/message/message_show_list.sql
+++ DML/message/message_show_list.sql
@@ -1,5 +1,32 @@
--- 메세지 목록 확인
-SELECT message_id
-     , title
+-- 받은 쪽지함
+SELECT
+    sender_id as '보낸 사용자',
+    title as '쪽지 제목',
+    sent_at as '보낸 시간',
+    message_expiry_date as '쪽지 만료일',
+    CASE
+        WHEN check_time IS NULL THEN '읽지 않음'
+        ELSE '읽음'
+        END AS '읽음 여부'
   FROM message
- WHERE receiver_id = 'user01';
+ WHERE receiver_id = 'user04';
+
+-- 안 읽은 쪽지함
+SELECT
+    sender_id AS '보낸 사용자',
+    title AS '쪽지 제목',
+    sent_at AS '보낸 시간'
+FROM message
+WHERE receiver_id = 'user04'
+  AND check_time IS NULL;
+
+
+-- 보낸 쪽지함
+SELECT
+       receiver_id as '받는 사용자'
+     , title as '쪽지 제목'
+     , sent_at as '보낸 시간'
+FROM message
+WHERE sender_id = 'user03';
+
+select * from message;
